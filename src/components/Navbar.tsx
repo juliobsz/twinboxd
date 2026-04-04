d91d56861ae0823b8c70f66363e0e88e48b620a2
juliobsz <juliobrsouza@outlook.com>
2026-04-04 00:51:24 -0300

    Added a new logo to both navbar and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,19 @@ const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 right-0 z-40 border-b border-border/50 bg-background/80 backdrop-blur-xl">
             <div className="container mx-auto flex h-14 items-center justify-between px-4">
-                <a href="/" className="flex items-center gap-2 group">
-                    <span className="text-sm font-semibold tracking-tight text-foreground">
-            Letterboxd Match
-          </span>
+                <a href="/" className="flex items-center gap-2 group items-center justify-center">
+                    <img src="/letterboxd-match-no-bg.png" alt="logo" className="h-18 w-18"/>
+                    <span className="text-2xl font-bold tracking-tight text-foreground">
+                        Letterboxd Match
+                    </span>
                 </a>
                 <a
                     href="https://github.com/juliobsz/letterboxd-match"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-xs text-muted-foreground hover:text-foreground transition-colors"
+                    className="text-md text-muted-foreground hover:text-foreground transition-colors"
                 >
-                    GitHub
+                    Github
                 </a>
             </div>
         </header>
